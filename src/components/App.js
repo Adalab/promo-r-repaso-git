5332b9d8119d87c242498c66f033d6e806377c86
Marta Castrillo <mart.castrillo@gmail.com>
2022-11-30 13:48:56 +0100

    add mas texto

--- src/components/App.js
+++ src/components/App.js
@@ -141,7 +141,7 @@ function App() {
             </select>
           </label>
         </form>
-        <p>Holaaaa maricarmenes</p>
+        <p>Holaaaa maricarmenes os quiero mucho a todas</p>
         <section className="data">
           <table className="table">
             <thead>
