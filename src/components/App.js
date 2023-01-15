cebde1503b7f2239cec48ce7da6e1b6f37bb161b
Yara <yara.gordillo@gmail.com>
2023-01-15 15:22:46 +0100

    token·

--- src/components/App.js
+++ src/components/App.js
@@ -105,49 +105,49 @@ function App() {
   // HTML EN EL RETURN
 
   return (
-    <div className="App page">
-      <header className="header">
-        <h1 className="title--big">Adalabers</h1>
+    <div className='App page'>
+      <header className='header'>
+        <h1 className='title--big'>Adalabers</h1>
       </header>
 
       <main>
-        <form className="filters">
-          <label className="filters__text" htmlFor="counselor">
+        <form className='filters'>
+          <label className='filters__text' htmlFor='counselor'>
             Escoge una tutora:
             <select
               onChange={handleChangeCounselorFilter}
               value={counselorFilter}
-              className="form__input-text"
-              name="counselor"
-              id="counselor"
+              className='form__input-text'
+              name='counselor'
+              id='counselor'
             >
-              <option value="">Todos</option>
-              <option value="Yanelis">Yanelis</option>
-              <option value="Dayana">Dayana</option>
-              <option value="Iván">Iván</option>
-              <option value="Miguel">Miguel</option>
+              <option value=''>Todos</option>
+              <option value='Yanelis'>Yanelis</option>
+              <option value='Dayana'>Dayana</option>
+              <option value='Iván'>Iván</option>
+              <option value='Miguel'>Miguel</option>
             </select>
           </label>
-          <label className="filters__text" htmlFor="counselor">
+          <label className='filters__text' htmlFor='counselor'>
             Escoge una alumna
             <select
               // onChange={handleChangeCounselorFilter}
               // value={counselorFilter}
-              className="form__input-text"
-              name="counselor"
-              id="counselor"
+              className='form__input-text'
+              name='counselor'
+              id='counselor'
             >
-              <option value="">Todes</option>
-              <option value="Yanelis"> maripuri querida</option>
-              <option value="Dayana">maricarmen querida</option>
-              <option value="Iván">x</option>
-              <option value="Miguel">maria</option>
+              <option value=''>Todes</option>
+              <option value='Yanelis'> maripuri querida</option>
+              <option value='Dayana'>maricarmen querida</option>
+              <option value='Iván'>x</option>
+              <option value='Miguel'>maria</option>
             </select>
           </label>
         </form>
         <p>Hola mis perris Maricarmenes os quiero mucho a todas </p>
-        <section className="data">
-          <table className="table">
+        <section className='data'>
+          <table className='table'>
             <thead>
               <tr>
                 <th>Nombre</th>
@@ -159,44 +159,44 @@ function App() {
             <tbody>{renderList(filteredAdalabers)}</tbody>
           </table>
         </section>
-        <section className="add">
-          <h2 className="add_title tittle--big">Añadir una Adalaber</h2>
+        <section className='add'>
+          <h2 className='add_title tittle--big'>Añadir una Adalaber</h2>
           <form onSubmit={handleSummit}>
-            <label className="add__label" htmlFor="newName">
+            <label className='add__label' htmlFor='newName'>
               Nombre:
               <input
                 onChange={handleChangeInput}
                 value={newAdalaber.name}
-                type="text"
-                className="form__input-text"
-                name="name"
-                id="newName"
+                type='text'
+                className='form__input-text'
+                name='name'
+                id='newName'
               />
             </label>
-            <label className="add__label" htmlFor="newCounselor">
+            <label className='add__label' htmlFor='newCounselor'>
               Tutora:
               <input
                 onChange={handleChangeInput}
                 value={newAdalaber.counselor}
-                type="text"
-                className="form__input-text"
-                name="counselor"
-                id="newCounselor"
+                type='text'
+                className='form__input-text'
+                name='counselor'
+                id='newCounselor'
               />
             </label>
-            <label className="add__label" htmlFor="newSpeciality">
+            <label className='add__label' htmlFor='newSpeciality'>
               Especialidad:
               <input
                 onChange={handleChangeInput}
                 value={newAdalaber.speciality}
-                type="text"
-                className="form__input-text"
-                name="speciality"
-                id="newSpeciality"
+                type='text'
+                className='form__input-text'
+                name='speciality'
+                id='newSpeciality'
               />
             </label>
-            <label className="add__label">
-              <button onClick={handleClickNewAdalaber} className="form__btn">
+            <label className='add__label'>
+              <button onClick={handleClickNewAdalaber} className='form__btn'>
                 Añadir una nueva Adalaber
               </button>
             </label>
